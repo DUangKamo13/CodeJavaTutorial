4559969b4f08514f849aeda5704d32490db02983
DUangKamo13 <152241366+DUangKamo13@users.noreply.github.com>
2024-08-01 09:43:09 +0700

    Update TestFor.java

--- TestFor.java
+++ TestFor.java
@@ -1,7 +1,9 @@
 public class TestFor {
     public static void main(String[] args) {
+        int totol;
         for(int i = 1; i <= 12; i++){
-            System.out.println(2+ "*" +i+"="+2*i);
+            totol = 2*i;
+            System.out.println("2 X " +i+" = "+totol);
         }
     }
 }

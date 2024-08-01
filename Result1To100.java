bf30e3fe1a39875958dd48c914320494c1b467cc
DUangKamo13 <152241366+DUangKamo13@users.noreply.github.com>
2024-08-01 10:23:15 +0700

    Update Result1To100.java

--- Result1To100.java
+++ Result1To100.java
@@ -2,10 +2,9 @@ public class Result1To100 {
     public static void main(String[] args) {
         int Result = 1 ,sum = 0;
         while(Result <= 100){
-            System.out.println("Result"+Result);
-            Result++;
             sum += Result;
+            Result++;
         }
-        System.out.println("result : "+sum);
+        System.out.println("result 1 To 100 : "+sum);
     }
 }

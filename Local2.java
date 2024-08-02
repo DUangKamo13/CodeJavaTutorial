31a8f56bc483b934ccc11e68bfc707b99e425fac
DUangKamo13 <152241366+DUangKamo13@users.noreply.github.com>
2024-08-02 13:24:50 +0700

    Create Local2.java

---
+++ Local2.java
@@ -0,0 +1,11 @@
+public class Local2 {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("x = "+x);
+        one();
+    }
+    public static void one() {
+        int x = 3;
+        System.out.println("x = "+x);
+    }
+}

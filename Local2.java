0d5a92b579c44c56a7e54e9abc53f98cfdffbe6f
DUangKamo13 <152241366+DUangKamo13@users.noreply.github.com>
2024-08-02 13:25:58 +0700

    Update Local2.java

--- Local2.java
+++ Local2.java
@@ -3,6 +3,7 @@ public class Local2 {
         int x = 5;
         System.out.println("x = "+x);
         one();
+        System.out.println("x = "+x);
     }
     public static void one() {
         int x = 3;

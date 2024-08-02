cbd401b4d1ab2e6b27f2f1f630c356c7a44f073c
DUangKamo13 <152241366+DUangKamo13@users.noreply.github.com>
2024-08-02 10:00:45 +0700

    Update LoopCall.java

--- LoopCall.java
+++ LoopCall.java
@@ -4,6 +4,7 @@ public class LoopCall {
         for(int i = 1; i <= 5; i++){
             displayMessage();
         }
+        System.out.println("back in the main method.");
     }
     public static void displayMessage() {
         System.out.println("Hello from the displayMessage");

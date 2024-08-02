c329e6a6c257d96a05719304c3b5749dae3dd678
DUangKamo13 <152241366+DUangKamo13@users.noreply.github.com>
2024-08-02 09:37:38 +0700

    Create SimpleMethod.java

---
+++ SimpleMethod.java
@@ -0,0 +1,9 @@
+public class SimpleMethod {
+    public static void main(String[] args) {
+        System.out.println("Hello from the main method.");
+        displayMessage();
+    }
+    public static void displayMessage() {
+        System.out.println("Hello from the displayMessage method. ");
+    }
+}
